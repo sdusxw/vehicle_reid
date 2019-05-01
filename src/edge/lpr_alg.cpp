5fe6c69de4ef294bf1afecbd5141854411114860
sdusxw <sxw@sdboon.com>
2019-05-01 16:15:47 +0800

    edgex4

--- src/edge/lpr_alg.cpp
+++ src/edge/lpr_alg.cpp
@@ -1,5 +1,6 @@
 #include "lpr_alg.h"
 #include "TH_PlateID.h"
+#include "jpg_codec.h"
 
 #include <stdio.h>
 
@@ -28,6 +29,8 @@ bool vlpr_init()
     c_Config.bUTF8 = 1;
     c_Config.bLotDetect = 0;
     c_Config.bLeanCorrection = 1;
+    c_Config.nOrderOpt = 2;
+    c_Config.nImproveSpeed = 0;
 
     
     int nRet = TH_InitPlateIDSDK(&c_Config);
@@ -57,5 +60,38 @@ bool vlpr_init()
 //车牌识别，输入为jpg图像指针和长度，输出识别结果，识别结果结构体需要调用方分配
 bool vlpr_analyze(const unsigned char *pImage, int len, PVPR pVPR)
 {
+    int w=0;int h=0; int c=3;
+    unsigned char *rgb_buf = (unsigned char *)malloc(4096*2160*3);
+    BoonJpegCodec bjc;
+    clock_t t=clock();
+    bool ret = bjc.JpegUnCompress((char *)pImage, len, (char *)rgb_buf,
+                                  4096*2160*3, w, h, c);
+    if(!ret)
+        return false;
+    clock_t tt = clock() - t;
+    std::cout << "Jpeg w:\t" << w <<"\th:\t" << h << "\tTime:\t" << tt/1000 << "ms" << std::endl;
+    TH_PlateIDResult result[6];
+    int nResultNum=1;
+    int nRet=TH_RecogImage(rgb_buf, w, h,  result, &nResultNum, NULL, &c_Config);
+    printf("ret=%d cnt=%d \n", nRet, nResultNum);
+    
+    if(nRet!=0)
+    {
+        printf("RecogImageRet = %d\n", nRet);
+        return false;
+    }
+    
+    if(nRet == 0)
+    {
+        for(int i=0; i<nResultNum; i++)
+        {
+            // memcpy(&nFrame,  result[i].pbyBits, 8);
+            //printf("nFrame=%I64d\n", nFrame);
+            if(result[i].nType>=0)
+                printf("plate: %s confidence:%d time:%d\n", result[i].license, result[i].nConfidence, result[i].nTime);
+            else
+                printf("Type: %d\n", result[i].nType);
+        }
+    }
     return true;
 }
--- src/edge/lpr_test.cpp
+++ src/edge/lpr_test.cpp
@@ -1,7 +1,37 @@
 #include "lpr_alg.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
+using namespace std;
+
+int main(int argc, char ** argv)
 {
-    vlpr_init();
+    if(argc < 2)
+    {
+        cout << argv[0] << " filename" << endl;
+        return 0;
+    }
+    if(vlpr_init())
+    {
+        cout<<"LPR_ALG init OK"<<endl;
+        std::ifstream file(argv[1], std::ios::in | std::ios::binary | std::ios::ate);
+        if (!file.is_open()) {
+            fprintf(stderr, "open file fail: %s\n", argv[1]);
+            return -1;
+        }
+        
+        std::streampos size = file.tellg();
+        file.seekg(0, std::ios::beg);
+        std::string buffer(size, ' ');
+        file.read(&buffer[0], size);
+        file.close();
+        VPR vpr;
+        vlpr_analyze((const unsigned char *)buffer.c_str(), (int)size, &vpr);
+        
+    }else{
+        cout<<"LPR_ALG init Fail, Exit"<<endl;
+    }
+    
     return 0;
 }
